7474eef0e0510298db663a085e717966338647b0
Harshvardhan <harshvardhansinghtanwar443@gmail.com>
2026-01-29 20:02:31 +0530

    Add files via upload

---
+++ string_reversing_question.py
@@ -0,0 +1,59 @@
+# 1
+s = "PythonProgramming"
+print(s[0:6])          
+
+# 2
+s = "DataScience"
+print(s[4:])           
+
+# 3
+s = "MachineLearning"
+print(s[0:7])          
+
+# 4
+s = "Developer"
+print(s[3:])           
+
+# 5
+s = "Artificial"
+print(s[:4])           
+
+# 6
+s = "Notebook"
+print(s[-3:])          
+
+# 7
+s = "Engineering"
+print(s[0:6])          
+
+# 8
+s = "Instagram"
+print(s[-4:])          
+
+# 9
+s = "Coding"
+print(s[:-2])     
+
+# 10
+s = "Computer"
+print(s[1:6])          
+
+# 11
+s = "LearningPython"
+print(s[8:])          
+
+# 12
+s = "DataAnalytics"
+print(s[4:])           
+
+# 13
+s = "Technology"
+print(s[:4])           
+
+# 14
+s = "University"
+print(s[4:8])          
+
+# 15
+s = "Programming"
+print(s[3:7])          
